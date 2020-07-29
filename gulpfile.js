4f8a21709c42dd3c86c04053ca93567fa12ca297
olgasmoczynska <53126593+olgasmoczynska@users.noreply.github.com>
2020-07-30 00:33:30 +0200

    Create the project structure and add config files

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const sourcemaps = require('gulp-sourcemaps');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
 
-gulp.task('sass', function () {
-    return gulp.src('scss/style.scss')
+gulp.task("sass", function () {
+    return gulp
+        .src("sass/main.scss")
         .pipe(sourcemaps.init())
         .pipe(sass({
-            errLogToConsole: true,
-            outputStyle: 'expanded'
-        }))
+            outputStyle: "expanded"
+        }).on("error", sass.logError))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest("css"));
 });
 
-gulp.task('watch', function () {
-    gulp.watch('scss/**/*.scss', ['sass']);
+gulp.task("watch", function () {
+    gulp.watch("sass/**/*.scss", gulp.series("sass"));
 });
